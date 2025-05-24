7427208de75437d8924cead2c9f8f5646fffc130
King1020-1 <king1020q@gmail.com>
2025-05-24 12:00:00 +0530

    Added Project 016

---
+++ Intermediate-Projects/Day-016-Coffee-Machine-with-OOP/README.md
@@ -0,0 +1,23 @@
+# Day 16 - Coffee Machine with OOP
+
+This is my Day 16 Project
+
+---
+
+## Description
+
+The task is same as Day-015  
+**Requirements:** Same as Day 15, but implemented using classes.  
+[Requirements Link](Coffee-Machine-Requirements.pdf)
+**Solution URL:** [Coffee Machine OOP](main.py)
+
+---
+
+## Learnings
+
+1. Reading documentations to know how functions work.
+2. Learned how to use and interact with multiple classes in a project.
+3. Improved code organization and readability by separating logic into `Menu`, `MenuItem`, `CoffeeMaker`, and `MoneyMachine` classes.
+4. Understood the benefits of OOP for scalability and maintainability in larger projects.
+
+---
---
+++ Intermediate-Projects/Day-016-Coffee-Machine-with-OOP/main.py
@@ -0,0 +1,25 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+while True:
+    choice = input(f"What would you like? ({menu.get_items()}report): ").lower()
+    if choice == "off":
+        break
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        if menu.find_drink(choice) is None:
+            continue
+        else:
+            drink = menu.find_drink(choice)
+        if not (coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost)) :
+            continue
+        coffee_maker.make_coffee(drink)
+
